ba597b5213ef2e4e0a1553780b57cab04bd599b6
Scott Davies <scott@scottdavies.uk>
2023-03-15 14:01:24 +0000

    updated as per context step

---
+++ src/components/saved-colors/saved-color.tsx
@@ -0,0 +1,37 @@
+import { MouseEventHandler } from 'react';
+import { AdjustColorActions } from '../../color-reducer';
+import Button from '../shared/button';
+import ColorChangeSwatch from '../shared/color-change-swatch';
+
+type SavedColorProps = {
+  name: string;
+  hexColor: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  onRemove?: () => void;
+  dispatch: React.Dispatch<AdjustColorActions>;
+};
+
+const SavedColor = ({
+  name,
+  hexColor,
+  onClick,
+  onRemove,
+  dispatch,
+}: SavedColorProps) => {
+  return (
+    <article className="flex place-content-between items-center gap-2">
+      <ColorChangeSwatch
+        hexColor={hexColor}
+        onClick={() => {
+          dispatch({ type: 'update-hex-color', payload: { hexColor } });
+        }}
+      />
+      <h3 className="whitespace-nowrap text-sm">{name}</h3>
+      <Button variant="destructive" size="small" onClick={onRemove}>
+        Remove
+      </Button>
+    </article>
+  );
+};
+
+export default SavedColor;
